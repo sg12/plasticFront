ca7403e493e3d51c45643f2840b77f33c01e51ef
Malik Anton <antosha-malik@yandex.ru>
2023-11-07 23:22:21 +0700

    Add file structure and articles
    Add libraries: axios, sass and normalize.

---
+++ src/components/app/App.jsx
@@ -0,0 +1,15 @@
+import 'normalize.css'
+import './App.scss'
+
+import Articles from '../articles/Articles'
+
+function App() {
+
+	return (
+		<>
+			<Articles />
+		</>
+	)
+}
+
+export default App
