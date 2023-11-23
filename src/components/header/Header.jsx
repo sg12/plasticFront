e35c667cacf67c52e099a3c6b397cf04227e62f2
bagukan74 <112675947+Bakugan74@users.noreply.github.com>
2023-11-23 14:11:16 +0700

    fix header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
@@ -6,53 +6,34 @@ import "./Header.scss";
 import logo from '../../assets/imgs/logo.png';
 
 const Header = () => {
-	// const [activeLink, setActiveLink] = useState(0);
+	const [activeLink, setActiveLink] = useState(0);
 
-	// const handleLinkClick = (index, event) => {
-	// 	setActiveLink(index);
-	// 	event.preventDefault();
-	// };
+	const handleLinkClick = (index) => {
+		setActiveLink(index);
+	};
 
-	// const links = [
-	// 	{ text: 'Главная', to: '/' },
-	// 	{ text: 'Услуги', to: '/services' },
-	// 	{ text: 'Врачи', to: '/doctors' },
-	// 	{ text: 'Клиники', to: '/clinics' },
-	// 	{ text: 'Отзывы', to: '/reviews' },
-	// 	{ text: 'Статьи', to: '/articles' },
-	// ];
+	const links = [
+		{ text: 'Главная', to: '/' },
+		{ text: 'Услуги', to: '/services' },
+		{ text: 'Врачи', to: '/doctors' },
+		{ text: 'Клиники', to: '/clinics' },
+		{ text: 'Отзывы', to: '/reviews' },
+		{ text: 'Статьи', to: '/articles' },
+	];
 
 	return (
 		<header className="header">
 			<div className="header__logo">
-				<img className='logo__img' src={logo} alt="лого" />
+			<Link to='/'> <img className='logo__img' src={logo} alt="лого" /></Link>
 			</div>
 			<ul className="header__links">
-				{/* {links.map((link, index) => (
+				{links.map((link, index) => (
 					<li key={index} className={index === activeLink ? 'active' : ''}>
 						<Link to={link.to} onClick={(event) => handleLinkClick(index, event)}>
 							{link.text}
 						</Link>
 					</li>
-				))} */}
-				<li>
-					<Link to='/' >Главная</Link>
-				</li>
-				<li>
-					<Link to='/services' >Услуги</Link>
-				</li>
-				<li>
-					<Link to='/doctors' >Врачи</Link>
-				</li>
-				<li>
-					<Link to='/clinics' >Клиники</Link>
-				</li>
-				<li>
-					<Link to='/reviews' >Отзывы</Link>
-				</li>
-				<li>
-					<Link to='/articles' >Статьи</Link>
-				</li>
+				))}
 				<li>
 					<div className="header__button-item">
 						<p>Войти</p>
