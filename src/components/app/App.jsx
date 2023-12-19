93822f84f00b5251220c0276756aeb433de7ec0e
Motya <111338107+vlnt-trsv@users.noreply.github.com>
2023-12-19 20:31:18 +0700

    Update `App.jsx`

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,46 +1,44 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import 'normalize.css';
-import './App.scss';
+import "normalize.css";
+import "./App.scss";
 
-import Layout from '../../pages/Layout';
-import MainPage from '../../pages/MainPage';
-import ServicesPage from '../../pages/ServicesPage';
-import DoctorsPage from '../../pages/DoctorsPage';
-import ClinicsPage from '../../pages/ClinicsPage';
-import ReviewsPage from '../../pages/ReviewsPage';
-import ArticlesPage from '../../pages/ArticlesPage';
-import NotFound from '../notFound/NotFound';
+import Layout from "../../pages/Layout";
+import MainPage from "../../pages/MainPage";
+import ServicesPage from "../../pages/ServicesPage";
+import DoctorsPage from "../../pages/DoctorsPage";
+import ClinicsPage from "../../pages/ClinicsPage";
+import ReviewsPage from "../../pages/ReviewsPage";
+import ArticlesPage from "../../pages/ArticlesPage";
+import NotFound from "../notFound/NotFound";
 import AccountPage from "../../pages/AccountPage";
 
 // Account - `AsidePanel.jsx`
-import ProfileInfo from '../navBars/ProfileInfo/ProfileInfo';
+import ProfileInfo from "../navBars/ProfileInfo/ProfileInfo";
 // import ServicesInfo from '../navBars/ServicesInfo/ServicesInfo';
 // import ClinicsInfo from '../navBars/clinicsInfo/ClinicsInfo';
 import FavouritesInfo from '../navBars/favouritesInfo/FavouritesInfo';
-import SettingsInfo from '../navBars/settingsInfo/SettingsInfo';
-
-
+import EditUser from "../navBars/editUser/EditUser";
 
 function App() {
-
-	return (
-		<>
-			<Routes>
-				<Route path='/' element={<Layout />}>
-					<Route index element={<MainPage />}></Route>
-					<Route path='services' element={<ServicesPage />}></Route>
-					<Route path='doctors' element={<DoctorsPage />}></Route>
-					<Route path='clinics' element={<ClinicsPage />}></Route>
-					<Route path='reviews' element={<ReviewsPage />}></Route>
-					<Route path='articles' element={<ArticlesPage />}></Route>
-					<Route path='*' element={<NotFound />}></Route>
-				</Route>
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />}></Route>
+          <Route path="services" element={<ServicesPage />}></Route>
+          <Route path="doctors" element={<DoctorsPage />}></Route>
+          <Route path="clinics" element={<ClinicsPage />}></Route>
+          <Route path="reviews" element={<ReviewsPage />}></Route>
+          <Route path="articles" element={<ArticlesPage />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Route>
 
 				<Route path="account" element={<AccountPage />}>
-					<Route path="profile" element={<ProfileInfo />} />
+					<Route path="profile" element={<ProfileInfo />}>
+             <Route path="edit" element={<EditUser />}/>
+          </Route>
 					<Route path="favourites" element={<FavouritesInfo />} />
-					<Route path='settings' element={<SettingsInfo />}></Route>
 				</Route>
 			</Routes>
 		</>
