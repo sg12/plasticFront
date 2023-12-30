d81963550415bb05b17437f5493bf02b87d756ac
Malik Anton <antosha-malik@yandex.ru>
2023-12-30 18:08:00 +0700

    Fix ui

--- src/components/enterItem/enterItem.jsx
+++ src/components/enterItem/enterItem.jsx
@@ -1,28 +1,28 @@
 import { Link } from 'react-router-dom';
-import FieldButton from '../UI/button/fieldButton/FieldButton';
+import FieldButton from '../UI/buttons/fieldButton/FieldButton';
 
 import './EnterItem.scss';
 
 const EnterItem = () => {
-    return (
-        <div className='enter'>
-            <div className='enter__content content'>
-                <p className='enter__title'>Выберите тип входа</p>
-                <Link to={'enterClient'}>
-                    <FieldButton>
-                        Клиент
-                    </FieldButton>
-                </Link>
-                <p className='enter__text'>или</p>
-                <Link to={'enterPartner'}>
-                    <FieldButton>
-                        Партнер
-                    </FieldButton>
-                </Link>
-                <Link to={'/'} href="#" className='enter__link'>Вернуться на главную</Link>
-            </div>
-        </div>
-    );
+	return (
+		<div className='enter'>
+			<div className='enter__content content'>
+				<p className='enter__title'>Выберите тип входа</p>
+				<Link to={'enterClient'}>
+					<FieldButton>
+						Клиент
+					</FieldButton>
+				</Link>
+				<p className='enter__text'>или</p>
+				<Link to={'enterPartner'}>
+					<FieldButton>
+						Партнер
+					</FieldButton>
+				</Link>
+				<Link to={'/'} href="#" className='enter__link'>Вернуться на главную</Link>
+			</div>
+		</div>
+	);
 };
 
 export default EnterItem;
