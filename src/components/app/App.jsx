ff903952d9bf288c01d9e2e4f6972a497bb46160
Valentin <111338107+vlnt-trsv@users.noreply.github.com>
2024-01-11 18:29:38 +0700

    Update App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -33,46 +33,46 @@ import ForgotPassword from "../forgotPassword/ForgotPassword";
 import NewPassword from "../newPassword/newPassword";
 import EnterPage from "../../pages/EnterPage";
 import AccountPage from "../../pages/AccountPage";
-import ArticleDetailedPage from '../../pages/ArticleDetailedPage';
+import ArticleDetailedPage from "../../pages/ArticleDetailedPage";
 
 function App() {
-	return (
-		<>
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					<Route index element={<MainPage />}></Route>
-					<Route path="services" element={<ServicesPage />}></Route>
-					<Route path="doctors" element={<DoctorsPage />}></Route>
-					<Route path="clinics" element={<ClinicsPage />}></Route>
-					{/* <Route path='stocks' element={<StocksPage />}></Route> */}
-					<Route path="articles" element={<ArticlesPage />}></Route>
-					<Route path='articles/:id' element={<ArticleDetailedPage />}></Route>
-					<Route path="*" element={<NotFound />}></Route>
-				</Route>
-				<Route path="enterPage" element={<EnterPage />}>
-					<Route index element={<EnterItem />}></Route>
-					<Route path="enterClient" element={<EnterClient />}></Route>
-					<Route path="registerClient" element={<RegisterClient />}></Route>
-					<Route path="enterPartner" element={<EnterPartner />}></Route>
-					<Route path="registerDoctor" element={<RegisterDoctor />}></Route>
-					<Route path="registerClinic" element={<RegisterClinic />}></Route>
-					<Route path="forgotPassword" element={<ForgotPassword />}></Route>
-					<Route path="newPassword" element={<NewPassword />}></Route>
-				</Route>
-				<Route path="account" element={<AccountPage />}>
-					<Route path="profile" element={<ProfileUser />} />
-					<Route path="reviews" element={<ReviewsInfo />} />
-					<Route path="help" element={<HelpInfo />} />
-					<Route path="support" element={<SupportInfo />} />
-					<Route path="clinic" element={<ClinicsInfo />} />
-					<Route path="service" element={<ServicesInfo />} />
-					<Route path="specialist" element={<SpecialistInfo />} />
-					<Route path="settings" element={<SettingsInfo />} />
-					<Route path="favourites" element={<FavouritesInfo />} />
-				</Route>
-			</Routes>
-		</>
-	);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />}></Route>
+          <Route path="services" element={<ServicesPage />}></Route>
+          <Route path="doctors" element={<DoctorsPage />}></Route>
+          <Route path="clinics" element={<ClinicsPage />}></Route>
+          {/* <Route path='stocks' element={<StocksPage />}></Route> */}
+          <Route path="articles" element={<ArticlesPage />}></Route>
+          <Route path="articles/:id" element={<ArticleDetailedPage />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Route>
+        <Route path="enterPage" element={<EnterPage />}>
+          <Route index element={<EnterItem />}></Route>
+          <Route path="enterClient" element={<EnterClient />}></Route>
+          <Route path="registerClient" element={<RegisterClient />}></Route>
+          <Route path="enterPartner" element={<EnterPartner />}></Route>
+          <Route path="registerDoctor" element={<RegisterDoctor />}></Route>
+          <Route path="registerClinic" element={<RegisterClinic />}></Route>
+          <Route path="forgotPassword" element={<ForgotPassword />}></Route>
+          <Route path="newPassword" element={<NewPassword />}></Route>
+        </Route>
+        <Route path="account" element={<AccountPage />}>
+          <Route path="profile" element={<ProfileUser />} />
+          <Route path="reviews" element={<ReviewsInfo />} />
+          <Route path="help" element={<HelpInfo />} />
+          <Route path="support" element={<SupportInfo />} />
+          <Route path="clinic" element={<ClinicsInfo />} />
+          <Route path="service" element={<ServicesInfo />} />
+          <Route path="specialist" element={<SpecialistInfo />} />
+          <Route path="settings" element={<SettingsInfo />} />
+          <Route path="favourites" element={<FavouritesInfo />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
