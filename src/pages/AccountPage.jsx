074afb78800ad87131d72618adfd13a32517aada
Valentin <111338107+vlnt-trsv@users.noreply.github.com>
2024-01-11 18:39:20 +0700

    Update AccountPage.jsx

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+// import { useAuth } from "../context/AuthContext";
+// import { Navigate } from "react-router-dom";
 import NavBars from "../components/navBars/NavBars";
 
 const AccountPage = () => {
+  // const { state } = useAuth();
+
+  // if (!state.isAuthenticated) {
+  //   return (
+  //     <>
+  //       <Navigate to="/enterPage" />
+  //     </>
+  //   );
+  // }
   return (
     <>
       <NavBars />
