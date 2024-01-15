5183167656a6368af9f1d07441cd92591dd300a0
armoney7 <arman.muradyan.2003@gmail.com>
2024-01-15 22:40:03 +0700

    fix ui

--- src/components/UI/buttons/whiteButton/WhiteButton.jsx
+++ src/components/UI/buttons/whiteButton/WhiteButton.jsx
@@ -1,11 +1,17 @@
 import classes from './WhiteButton.module.scss';
 
+import PropTypes from 'prop-types';
+
 const WhiteButton = ({children, ...props}) => {
     return (
         <button {...props} className={classes.licenseButton}>
             {children}
         </button>
     );
+};
+
+WhiteButton.propTypes = {
+	children: PropTypes.string.isRequired,
 };
 
 export default WhiteButton;
