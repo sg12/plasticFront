7512c24cb02e2c74f1c8d1bacac93461299a2f0d
Valentin <111338107+vlnt-trsv@users.noreply.github.com>
2024-01-25 18:16:44 +0700

    Create constuctor faces
    Создал триггеры на лице

--- src/components/constructor/Constructor_comp.jsx
+++ src/components/constructor/Constructor_comp.jsx
@@ -1,13 +1,18 @@
-import ConstructorFilter from '../constructorFilter/ConstructorFilter';
-import './Construcror_comp.scss';
+import { useState } from "react";
+import ConstructorFace from "../constructorFace/ConstructorFace";
+import ConstructorFilter from "../constructorFilter/ConstructorFilter";
+import "./Construcror_comp.scss";
 
 const Constructor = () => {
-	return (
-		<div>
-            <h1>Страница конструктора</h1>
-			<ConstructorFilter />
-        </div>
-	);
+  const [activeLine, setActiveLine] = useState(null);
+
+  return (
+    <div>
+      <h1>Страница конструктора</h1>
+      <ConstructorFilter />
+      <ConstructorFace activeLine={activeLine} setActiveLine={setActiveLine} />
+    </div>
+  );
 };
 
 export default Constructor;
