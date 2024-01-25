2172bf8d3cdb0f0cea834d949fc300be7d364e44
Nikomol <114502362+Nikomol@users.noreply.github.com>
2024-01-26 03:03:07 +0700

    add files "about operation"

--- src/components/constructor/Constructor_comp.jsx
+++ src/components/constructor/Constructor_comp.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ConstructorFace from "../constructorFace/ConstructorFace";
 import ConstructorFilter from "../constructorFilter/ConstructorFilter";
+import About_operation from "../about_operation/about_operation";
 import "./Construcror_comp.scss";
 
 const Constructor = () => {
@@ -11,6 +12,7 @@ const Constructor = () => {
       <h1>Страница конструктора</h1>
       <ConstructorFilter />
       <ConstructorFace activeLine={activeLine} setActiveLine={setActiveLine} />
+      <About_operation activeLine={activeLine}/>
     </div>
   );
 };
