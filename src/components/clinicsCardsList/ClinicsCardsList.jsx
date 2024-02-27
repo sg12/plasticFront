27642bd0fb33b17549e3daab32fac3990a04d5bf
Malik Anton <antosha-malik@yandex.ru>
2024-02-27 17:45:32 +0700

    Fix clinics

--- src/components/clinicsCardsList/ClinicsCardsList.jsx
+++ src/components/clinicsCardsList/ClinicsCardsList.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 import PlasticServices from '../../services/PlasticServices';
 
 import ClinicsCardsItem from '../clinicsCardsItem/ClinicsCardsItem';
-// import OutlineButton from '../UI/buttons/outlineButton/OutlineButton';
+import OutlineButton from '../UI/buttons/outlineButton/OutlineButton';
 import Spinner from '../spinner/Spinner';
 
 import { useFetching } from '../../hooks/useFetching';
@@ -14,14 +14,14 @@ const ClinicsCardsList = () => {
 
 	const [posts, setPosts] = useState([]);
 	const [offset, setOffset] = useState(0);
-	// const [totalCount, setTotalCount] = useState(0);
+	const [totalCount, setTotalCount] = useState(0);
 
 	const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
 		const response = await PlasticServices.getAllClinics(offset);
 		setPosts([...posts, ...response.data]);
 		setOffset(offset + 6);
-		// setTotalCount(response.headers['x-total-count']);
-		// console.log(response.headers);
+		setTotalCount(response.headers['x-total-count']);
+		console.log(response.headers['x-total-count']);
 	});
 
 	const onRequest = () => {
@@ -32,9 +32,9 @@ const ClinicsCardsList = () => {
 		onRequest();
 	}, []);
 
-	// const loadMorePosts = () => {
-	// 	onRequest();
-	// };
+	const loadMorePosts = () => {
+		onRequest();
+	};
 
 	const content = !(!isPostsLoading && !postError && posts.length === 0)
 		? posts.map((post) => (
@@ -50,9 +50,9 @@ const ClinicsCardsList = () => {
 		? <Spinner />
 		: null;
 
-	// const button = totalCount > posts.length || error || spinner
-	// 	? <OutlineButton className='component-button-text' onClick={loadMorePosts}>Показать ещё</OutlineButton>
-	// 	: null;
+	const button = totalCount > posts.length || error || spinner
+		? <OutlineButton className='component-button-text' onClick={loadMorePosts}>Показать ещё</OutlineButton>
+		: null;
 
 	return (
 		<section className='clinics-cards-list'>
@@ -63,7 +63,7 @@ const ClinicsCardsList = () => {
 				</ul>
 				{error}
 				{spinner}
-				{/* {button} */}
+				{button}
 			</div>
 		</section>
 	);
