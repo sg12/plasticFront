df11c4559e5c1d253b701de9b82e13359b598408
Valentin <111338107+vlnt-trsv@users.noreply.github.com>
2024-04-21 12:49:03 +0700

    update component

--- src/components/personalAccount/supportInfo/SupportInfo.jsx
+++ src/components/personalAccount/supportInfo/SupportInfo.jsx
@@ -1,41 +1,82 @@
+import React, { useState } from "react";
 import "./SupportInfo.scss";
 
 const SupportInfo = () => {
+  const [formData, setFormData] = useState({
+    requestTopic: "",
+    technicalDetails: "",
+    requestDescription: "",
+    files: null,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFileChange = (e) => {
+    setFormData({
+      ...formData,
+      files: e.target.files,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <div className="support">
       <span className="support__title">Обращение в службу поддержки</span>
       <span className="support__subtitle">Заполните форму</span>
-      <form className="support__form" action="">
-        {/* <div className="support__form-email">
-          <label htmlFor="">Почта</label>
-          <input type="text" placeholder="Введите вашу почту" />
-        </div> */}
-        {/* <div className="support__form-link">
-          <label htmlFor="">Ссылка</label>
-          <input type="text" placeholder="Ссылка на ваш профиль" />
-        </div> */}
+      <form className="support__form" onSubmit={handleSubmit}>
         <div className="support__form-request">
-          <label htmlFor="">Тема запроса</label>
-          <input type="text" placeholder="Какая проблема?" />
+          <label htmlFor="requestTopic">Тема запроса</label>
+          <input
+            type="text"
+            id="requestTopic"
+            name="requestTopic"
+            placeholder="Какая проблема?"
+            value={formData.requestTopic}
+            onChange={handleChange}
+          />
         </div>
         <div className="support__form-details">
-          <label htmlFor="">Технические детали</label>
-          <input type="text" placeholder="Название, версия браузера" />
+          <label htmlFor="technicalDetails">Технические детали</label>
+          <input
+            type="text"
+            id="technicalDetails"
+            name="technicalDetails"
+            placeholder="Название, версия браузера"
+            value={formData.technicalDetails}
+            onChange={handleChange}
+          />
         </div>
         <div className="support__form-description">
-          <label htmlFor="">Описание сути запроса</label>
-          <input type="text" placeholder="Опишите суть запроса" />
+          <label htmlFor="requestDescription">Описание сути запроса</label>
+          <input
+            type="text"
+            id="requestDescription"
+            name="requestDescription"
+            placeholder="Опишите суть запроса"
+            value={formData.requestDescription}
+            onChange={handleChange}
+          />
         </div>
-        {/* <div className="support__form-status">
-          <label htmlFor="">Ваш статус</label>
-          <input type="text" placeholder="Пациент, доктор, клиника" />
-        </div> */}
-      </form>
-      <form className="support__button" action="">
-        <input
-          type="file"
-          className="support__button-upload"
-        />
+        <div className="support__form-file">
+          <input
+            type="file"
+            id="fileInput"
+            name="files"
+            className="support__button-upload"
+            multiple
+            onChange={handleFileChange}
+          />
+        </div>
         <button type="submit" className="support__button-send">
           Отправить
         </button>
