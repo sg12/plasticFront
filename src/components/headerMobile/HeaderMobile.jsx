3a912be1ba7c572191326c3dcc6954ba00703ba4
Malik Anton <malikantonit@gmail.com>
2024-05-16 00:39:28 +0700

    Remove hover for footer and headerMobile. Fix adaptive for footer

--- src/components/headerMobile/HeaderMobile.jsx
+++ src/components/headerMobile/HeaderMobile.jsx
@@ -1,19 +1,17 @@
 import './HeaderMobile.scss';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import { links } from '../header/Header';
 
 import logo from '../../assets/icons/logo.png';
 import search from '../../assets/icons/search.png';
 
-import { Link, useLocation, NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const HeaderMobile = () => {
 
 	const [mobileNav, setMobileNav] = useState(false);
-
-	const [activeLink, setActiveLink] = useState(null);
 
 	const onOpenMobileNav = () => {
 		if (!mobileNav) {
@@ -22,18 +20,6 @@ const HeaderMobile = () => {
 			setMobileNav(false);
 		}
 	};
-
-	const handleLinkClick = (index) => {
-		setActiveLink(index);
-	};
-
-	const location = useLocation();
-
-	useEffect(() => {
-		const currentPath = window.location.pathname;
-		const foundLinkIndex = links.findIndex((link) => link.to === currentPath);
-		setActiveLink(foundLinkIndex === -1 ? null : foundLinkIndex);
-	}, [location.pathname]);
 
 	return (
 		<header className="header-mobile">
@@ -65,9 +51,8 @@ const HeaderMobile = () => {
 					{links.map((link, index) => (
 						<li key={index} className='header-mobile__link'>
 							<Link
-								className={`header-mobile__a ${index === activeLink ? 'header-mobile__a_active' : ''}`}
+								className={'header-mobile__a'}
 								to={link.to}
-								onClick={() => handleLinkClick(index)}
 							>
 								{link.text}
 							</Link>
