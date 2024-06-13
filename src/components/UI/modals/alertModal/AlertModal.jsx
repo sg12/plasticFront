7eacb6bec84eb88ba8efd2a07571dbb4a42db3b8
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-06-13 15:16:13 +0700

    fix ui component

--- src/components/UI/modals/alertModal/AlertModal.jsx
+++ src/components/UI/modals/alertModal/AlertModal.jsx
@@ -1,9 +1,14 @@
+import OutlineButton from "../../buttons/outlineButton/OutlineButton";
 import styles from "./AlertModal.module.scss";
 
 const AlertModal = ({ isOpen, onClose, onAccept, message, title }) => {
   return (
     isOpen && (
-        <div className={`${styles.alert__container} ${isOpen ? styles.alert__modal : ''}`}>
+      <div
+        className={`${styles.alert__container} ${
+          isOpen ? styles.alert__modal : ""
+        }`}
+      >
         <div className={styles.alert__content}>
           <span className={styles.alert__title}>{title}</span>
           <p className={styles.alert__subtitle}>{message}</p>
