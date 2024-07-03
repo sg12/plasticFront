d4c5cf0dd7c0c234041d8660b8c8b2a70c4dfafb
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-07-03 19:33:01 +0700

    Update PlasticServices

--- src/services/PlasticServices.js
+++ src/services/PlasticServices.js
@@ -3,96 +3,108 @@ import Cookies from "js-cookie";
 
 const _apiBase = import.meta.env.VITE_API_URL;
 
-// const _apiBase = "https://jsonplaceholder.typicode.com/";
-// const _apiBase = "http://localhost:8000/api/v1";
-
 // Функция для создания экземпляра axios с токеном из куки
 const createAxiosInstance = () => {
-	return axios.create({
-		baseURL: _apiBase,
-		headers: {
-			"Authorization": `Token ${Cookies.get("token")}`,
-			"Content-Type": "application/json",
-		},
-	});
+  return axios.create({
+    baseURL: _apiBase,
+    headers: {
+      Authorization: `Token ${Cookies.get("key")}`,
+      "Content-Type": "application/json",
+    },
+  });
 };
 
-const axiosInstance = createAxiosInstance();
+let axiosInstance = createAxiosInstance();
 
 class PlasticServices {
-	static async getAllArticles(page) {
-		const response = await axios.get(`${_apiBase}/articles?limit=6&page=${page}`);
-		return response;
-	}
+  static async getAllArticles(page) {
+    const response = await axios.get(
+      `${_apiBase}/articles?limit=6&page=${page}`
+    );
+    return response;
+  }
 
-	//! изменить формат времени
-	static async getArticle(id) {
-		const response = await axios.get(`${_apiBase}/articles/${id}`);
-		return response;
-	}
+  //! изменить формат времени
+  static async getArticle(id) {
+    const response = await axios.get(`${_apiBase}/articles/${id}`);
+    return response;
+  }
 
-	//! прокидывать значение через функцию или цикл
-	static async getAllClinics(limit, page, search, service, reception, sort) {
-		const response = await axios.get(`${_apiBase}/clinics?limit=${limit}&page=${page}&search=${search}&service=${service}&reception=${reception}&sort=${sort}`);
-		return response;
-	}
+  //! прокидывать значение через функцию или цикл
+  static async getAllClinics(limit, page, search, service, reception, sort) {
+    const response = await axios.get(
+      `${_apiBase}/clinics?limit=${limit}&page=${page}&search=${search}&service=${service}&reception=${reception}&sort=${sort}`
+    );
+    return response;
+  }
 
-	static async getClinic(id) {
-		const response = await axios.get(`${_apiBase}/clinics/${id}`);
-		return response;
-	}
+  static async getClinic(id) {
+    const response = await axios.get(`${_apiBase}/clinics/${id}`);
+    return response;
+  }
 
-	//! прокидывать значение через функцию или цикл
-	static async getAllDoctors(limit, page, search, specialtie, gender, category, rating, reception, sort) {
-		const response = await axios.get(`${_apiBase}/surgeons?limit=${limit}&page=${page}&search=${search}&specialtie=${specialtie}&gender=${gender}&category=${category}&rating=${rating}&reception=${reception}&sort=${sort}`);
-		return response;
-	}
+  //! прокидывать значение через функцию или цикл
+  static async getAllDoctors(
+    limit,
+    page,
+    search,
+    specialtie,
+    gender,
+    category,
+    rating,
+    reception,
+    sort
+  ) {
+    const response = await axios.get(
+      `${_apiBase}/surgeons?limit=${limit}&page=${page}&search=${search}&specialtie=${specialtie}&gender=${gender}&category=${category}&rating=${rating}&reception=${reception}&sort=${sort}`
+    );
+    return response;
+  }
 
-	static async getDoctor(id) {
-		const response = await axios.get(`${_apiBase}/surgeons/${id}`);
-		return response;
-	}
+  static async getDoctor(id) {
+    const response = await axios.get(`${_apiBase}/surgeons/${id}`);
+    return response;
+  }
 
-	static async getUser() {
-		const response = await axiosInstance.get("/account/");
-		return response;
-	}
+  static async getUser() {
+    const profile = await axiosInstance.get("/profile");
+    const profileMore = await axiosInstance.get(
+      `/profile/${profile.data.role}`
+    );
 
-	static async patchUser(editedData) {
-		const response = await axiosInstance.patch("/account/", { user: editedData }, { date_born: editedData?.date_born });
-		return response;
-	}
+    return {
+      data: {
+        ...profile.data,
+        ...profileMore.data,
+      },
+    };
+  }
 
-	static async getFaq() {
-		const response = await axios.get(`${_apiBase}/faq/`);
-		return response;
-	}
+  static async patchUser(editedData, role) {
+    return await axiosInstance.patch(`/profile/${role}`, { data: editedData });
+  }
 
-	static async registerUser(data, type) {
-		try {
-			const response = await axios.post(`${_apiBase}/auth/register/${type}/`, data);
-			return response.data; // Ответ от сервера (можно обработать по вашему усмотрению)
-		} catch (error) {
-			window.alert('Ошибка при регистрации: ' + error.message);
-			console.error('Ошибка при регистрации:', error);
-		}
-	}
+  static async getFaq() {
+    return await axiosInstance.get(`${_apiBase}/faq`);
+  }
 
-	static async loginUser(data) {
-		try {
-			const response = await axios.post(`${_apiBase}/auth/login/`, data);
-			return response.data; // Ответ от сервера (можно обработать по вашему усмотрению)
-		} catch (error) {
-			window.alert('Ошибка при авторизации: ' + error.message);
-			console.error('Ошибка при авторизации:', error);
-		}
-	}
+  static async registerUser(data, type) {
+    const response = await axiosInstance.post(
+      `${_apiBase}/auth/register/${type}`,
+      data
+    );
+    return Cookies.set("key", response.data.key);
+  }
 
-	static async logoutUser() {
-		const response = await axiosInstance.get("/auth/logout/");
-		Cookies.remove("token");
-		return response;
-	}
+  static async loginUser(data) {
+    const response = await axiosInstance.post(`${_apiBase}/auth/login`, data);
+    return Cookies.set("key", response.data.key);
+  }
+
+  static async logoutUser() {
+    await axiosInstance.get("/auth/logout");
+    return Cookies.remove("token");
+  }
 }
 
 export default PlasticServices;
