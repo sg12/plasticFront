a683e68c6d71efc6c3c3b5bc9c6f5550f6448c2c
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-07-03 19:31:55 +0700

    update Account Page

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,20 +1,8 @@
-// import { useAuth } from "../context/AuthContext";
-// import { Navigate } from "react-router-dom";
 import { useEffect } from "react";
-import PersonalAccount from "../components/personalAccount/PersonalAccount";
 import { UserProvider } from "../context/UserContext";
+import PersonalAccount from "../components/personalAccount/PersonalAccount";
 
 const AccountPage = () => {
-  // const { state } = useAuth();
-
-  // if (!state.isAuthenticated) {
-  //   return (
-  //     <>
-  //       <Navigate to="/enterPage" />
-  //     </>
-  //   );
-  // }
-
   useEffect(() => {
     document.body.style.background = "#f2f2f2";
     return () => (document.body.style.background = "");
