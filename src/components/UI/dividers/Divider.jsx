1f59707a8058b2ca67c0ae30feb054fa87d8b4a4
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-07-04 22:25:17 +0700

    update ui components

--- src/components/UI/dividers/Divider.jsx
+++ src/components/UI/dividers/Divider.jsx
@@ -1,10 +1,18 @@
-const Divider = ({ color, thickness, margin, opacity, andStyle }) => {
+const Divider = ({
+  color,
+  thickness,
+  margin,
+  marginTop,
+  opacity,
+  andStyle,
+}) => {
   const style = {
     backgroundColor: color || "#3066be",
     width: "100%",
     height: thickness || 1,
     margin: margin || 0,
-    opacity: opacity || .25,
+    marginTop: marginTop || 0,
+    opacity: opacity || 0.25,
   };
 
   return <div style={style || andStyle} />;
