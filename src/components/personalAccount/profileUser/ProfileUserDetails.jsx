732e7a91d43f6d70c3b7fec95195bac424bfb934
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-07-10 17:21:17 +0700

    update personal account components
    Теперь блок дополнительной информации можно удалять, изменился их дизайн

--- src/components/personalAccount/profileUser/ProfileUserDetails.jsx
+++ src/components/personalAccount/profileUser/ProfileUserDetails.jsx
@@ -3,11 +3,7 @@ import Field from "../../UI/fields/Field";
 const ProfileUserDetails = ({ userData, fields }) => (
   <div className="profile__details">
     {fields.map((field, index) => (
-      <Field
-        key={index}
-        label={field.label}
-        value={userData?.[field.value] || "Неизвестно"}
-      />
+      <Field key={index} label={field.label} values={userData?.[field.value]} />
     ))}
   </div>
 );
