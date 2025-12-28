6e815782785bf13845a6e056b853973b013eda6b
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2025-12-28 18:04:38 +0700

    fix(shared): ui

--- src/shared/ui/sidebar.tsx
+++ src/shared/ui/sidebar.tsx
@@ -15,8 +15,8 @@ import { PanelLeftIcon } from "lucide-react"
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
-const SIDEBAR_WIDTH = "16rem"
-const SIDEBAR_WIDTH_MOBILE = "18rem"
+const SIDEBAR_WIDTH = "18rem"
+const SIDEBAR_WIDTH_MOBILE = "100%"
 const SIDEBAR_WIDTH_ICON = "3rem"
 const SIDEBAR_KEYBOARD_SHORTCUT = "b"
 
