4c1dacfbb6fe5715cf0a4dbc8a58f9518d7ee5c6
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2026-01-12 22:15:12 +0900

    refactor(features/profile)

--- src/features/createProfile/hooks/useCreateProfileForm.ts
+++ src/features/createProfile/hooks/useCreateProfileForm.ts
@@ -58,6 +58,7 @@ export const useCreateProfileForm = () => {
       clinicLicense: "",
       directorName: "",
       directorPosition: "",
+      doctors: [],
       documents: {},
     },
     mode: "all",
